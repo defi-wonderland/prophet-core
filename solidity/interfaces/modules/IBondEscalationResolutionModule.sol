3b8fec893d3269843b965fce3ee21711b6b9cec5
moebius <132487952+0xmoebius@users.noreply.github.com>
2023-08-17 14:30:55 -0300

    chore: use the same solidity versions (#98)

--- solidity/interfaces/modules/IBondEscalationResolutionModule.sol
+++ solidity/interfaces/modules/IBondEscalationResolutionModule.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.16 <0.9.0;
+pragma solidity ^0.8.19;
 
 import {IOracle} from '../IOracle.sol';
 import {IResolutionModule} from './IResolutionModule.sol';
--- solidity/interfaces/modules/IPrivateERC20ResolutionModule.sol
+++ solidity/interfaces/modules/IPrivateERC20ResolutionModule.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.16 <0.9.0;
+pragma solidity ^0.8.19;
 
 //TODO: add getters
 
