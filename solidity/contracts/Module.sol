42bd61adc5db4247e49eedadabe63b9ee648ba0f
Gas <86567384+gas1cent@users.noreply.github.com>
2023-08-28 21:22:38 +0400

    docs: module natspec (#115)

--- solidity/contracts/Module.sol
+++ solidity/contracts/Module.sol
@@ -13,16 +13,28 @@ abstract contract Module is IModule {
     ORACLE = _oracle;
   }
 
+  /**
+   * @notice Checks that the caller is the oracle
+   */
   modifier onlyOracle() {
     if (msg.sender != address(ORACLE)) revert Module_OnlyOracle();
     _;
   }
 
+  /// @inheritdoc IModule
   function setupRequest(bytes32 _requestId, bytes calldata _data) public virtual onlyOracle {
     requestData[_requestId] = _data;
     _afterSetupRequest(_requestId, _data);
   }
 
+  /// @inheritdoc IModule
   function finalizeRequest(bytes32 _requestId, address _finalizer) external virtual onlyOracle {}
+
+  /**
+   * @notice The hook that is called after `setupRequest`
+   *
+   * @param _requestId The ID of the request
+   * @param _data The data of the request
+   */
   function _afterSetupRequest(bytes32 _requestId, bytes calldata _data) internal virtual {}
 }
--- solidity/interfaces/IModule.sol
+++ solidity/interfaces/IModule.sol
@@ -1,11 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-interface IModule {
-  error Module_OnlyOracle();
-  error Module_InvalidCaller();
+// TODO: Definition of base has to precede definition of derived contract
+// import {IOracle} from './IOracle.sol';
 
+/**
+ * @title Module
+ * @notice Abstract contract to be inherited by all modules
+ */
+interface IModule {
+  /*///////////////////////////////////////////////////////////////
+                              ERRORS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Thrown when the caller is not the oracle
+   */
+  error Module_OnlyOracle();
+
+  /*///////////////////////////////////////////////////////////////
+                              VARIABLES
+  //////////////////////////////////////////////////////////////*/
+  // function ORACLE() external view returns (IOracle _oracle);
+
+  /**
+   * @notice Returns the data of the request associated with the provided id
+   *
+   * @param _requestId The id of the request
+   * @return _requestData The data of the request
+   */
+  function requestData(bytes32 _requestId) external view returns (bytes memory _requestData);
+
+  /*///////////////////////////////////////////////////////////////
+                              LOGIC
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Stores the request data and runs module-specific hooks
+   *
+   * @param _requestId The ID of the request
+   * @param _data The data of the request
+   */
   function setupRequest(bytes32 _requestId, bytes calldata _data) external;
+
+  /**
+   * @notice Finalizes a given request and executes any additional logic set by the chosen modules
+   *
+   * @param _requestId The ID of the request being finalized
+   * @param _finalizer The address that initiated the finalization
+   */
   function finalizeRequest(bytes32 _requestId, address _finalizer) external;
 
   /**
@@ -14,5 +57,4 @@ interface IModule {
    * @return _moduleName The name of the module.
    */
   function moduleName() external view returns (string memory _moduleName);
-  function requestData(bytes32 _requestId) external view returns (bytes memory _data);
 }
