503bec219f12bb7a04e8651d3e5ee36c8bd4187f
Ardy <116569704+0xArdy@users.noreply.github.com>
2023-08-31 04:01:55 -0400

    docs: fix internal paths (#113)

--- build-docs.sh
+++ build-docs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+root_path="solidity/interfaces"
 # generate docs in a temporary directory
-FOUNDRY_PROFILE=docs forge doc --out tmp/opoo-technical-docs
+temp_folder="technical-docs"
+
+FOUNDRY_PROFILE=docs forge doc --out "$temp_folder"
 
 # edit generated summary not to have container pages
 # - [jobs](solidity/interfaces/jobs/README.md)
@@ -24,15 +27,32 @@ else
 fi
 # copy the generated SUMMARY, from the tmp directory, without the first 5 lines
 # and paste them after the Interfaces section on the original SUMMARY
-tail -n +5 tmp/opoo-technical-docs/src/SUMMARY.md >> docs/src/SUMMARY.md
+tail -n +5 $temp_folder/src/SUMMARY.md >> docs/src/SUMMARY.md
 
 # delete old generated interfaces docs
-rm -rf docs/src/solidity/interfaces
+rm -rf docs/src/$root_path
 # there are differences in cp and mv behavior between UNIX and macOS when it comes to non-existing directories
 # creating the directory to circumvent them
-mkdir -p docs/src/solidity/interfaces
+mkdir -p docs/src/$root_path
 # move new generated interfaces docs from tmp to original directory
-cp -R tmp/opoo-technical-docs/src/solidity/interfaces docs/src/solidity/
+cp -R $temp_folder/src/$root_path docs/src/solidity/
 
 # delete tmp directory
-rm -rf tmp
+rm -rf $temp_folder
+
+# function to replace text in all files (to fix the internal paths)
+replace_text() {
+    for file in "$1"/*; do
+        if [ -f "$file" ]; then
+            sed -i "s|$temp_folder/src/||g" "$file"
+        elif [ -d "$file" ]; then
+            replace_text "$file"
+        fi
+    done
+}
+
+# path to the base folder
+base_folder="docs/src/$root_path"
+
+# calling the function to fix the paths
+replace_text "$base_folder"
