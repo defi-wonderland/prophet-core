f425f7bab714ac676f0d580ae3a28799f9981645
Gas <86567384+gas1cent@users.noreply.github.com>
2023-09-06 16:01:55 +0400

    docs: format `ISequentialResolutionModule` natspec properly (#128)

--- solidity/interfaces/modules/ISequentialResolutionModule.sol
+++ solidity/interfaces/modules/ISequentialResolutionModule.sol
@@ -5,16 +5,22 @@ import {IOracle} from '../../interfaces/IOracle.sol';
 import {IResolutionModule} from './IResolutionModule.sol';
 
 interface ISequentialResolutionModule is IOracle {
-  /// @notice Thrown when the caller is not a valid sub-module
+  /**
+   * @notice Thrown when the caller is not a valid sub-module
+   */
   error SequentialResolutionModule_OnlySubmodule();
 
-  /// @notice Thrown when the function called is not implemented
+  /**
+   * @notice Thrown when the function called is not implemented
+   */
   error SequentialResolutionModule_NotImplemented();
 
-  /// @notice Returns the list of submodules
-  /// @param _startFrom The index to start from
-  /// @param _batchSize The number of submodules to return
-  /// @return _list The list of submodules
+  /**
+   * @notice Returns the list of submodules
+   * @param _startFrom The index to start from
+   * @param _batchSize The number of submodules to return
+   * @return _list The list of submodules
+   */
   function listSubmodules(
     uint256 _startFrom,
     uint256 _batchSize
