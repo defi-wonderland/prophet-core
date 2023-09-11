3ec0285b460a8294d11ed0c44880905967c9fec2
moebius <132487952+0xmoebius@users.noreply.github.com>
2023-09-11 11:25:31 -0300

    docs: natspec for `ArbitratorModule` (#132)
    Co-authored-by: Gas <86567384+gas1cent@users.noreply.github.com>

--- solidity/contracts/modules/ArbitratorModule.sol
+++ solidity/contracts/modules/ArbitratorModule.sol
@@ -8,8 +8,11 @@ import {IArbitrator} from '../../interfaces/IArbitrator.sol';
 import {Module} from '../Module.sol';
 
 contract ArbitratorModule is Module, IArbitratorModule {
-  // bit 0 and 1 dispute status
-  // bit 2 arbitration result
+  /**
+   * @notice The status of all disputes
+   * @dev The status is stored in a single uint256 using
+   * the rightmost bits 0 and 1 for ArbitrationStatus and bit 2 for arbitration result
+   */
   mapping(bytes32 _disputeId => uint256 _data) internal _disputeData;
 
   constructor(IOracle _oracle) Module(_oracle) {}
@@ -18,54 +21,36 @@ contract ArbitratorModule is Module, IArbitratorModule {
     return 'ArbitratorModule';
   }
 
-  // get the arbitrator address for a dispute (the same arbitrator is fixed for a given request)
+  /// @inheritdoc IArbitratorModule
   function decodeRequestData(bytes32 _requestId) public view returns (address _arbitrator) {
-    // Get the arbitrator address associated with the request id
     _arbitrator = abi.decode(requestData[_requestId], (address));
   }
 
-  // takes a request ID and return a bool indicating if the arbitrator has validated the dispute or not.
-  // always return false for pending/unknown disputes
+  /// @inheritdoc IArbitratorModule
   function isValid(bytes32 _disputeId) external view returns (bool _isValid) {
     uint256 _currentDisputeData = _disputeData[_disputeId];
-
-    // Is the arbitration resolved ? If so, return the _valid flag
-    if (_currentDisputeData & 2 == 2) return (_currentDisputeData >> 2) & 1 == 1;
-
-    // else false (dispute isn't resolved -either active or another dispute was resolved- or was never started)
+    _isValid = (_currentDisputeData & 2 == 2) ? (_currentDisputeData >> 2) & 1 == 1 : false;
   }
 
-  // Return the status (unknown/not existing, active, resolved) of a dispute
-  // An active dispute is only active "here" -> if the request is resolved, the "loosing" dispute are still marked
-  // as active (but never valid and the oracle status is Won or Lost)
+  /// @inheritdoc IArbitratorModule
   function getStatus(bytes32 _disputeId) external view returns (ArbitrationStatus _disputeStatus) {
     uint256 _currentDisputeData = _disputeData[_disputeId];
 
     _disputeStatus = ArbitrationStatus(_currentDisputeData & 3);
   }
 
-  // Gets the dispute from the pre-dispute module and opens it for resolution
-  // call the arbitrator with the dispute to arbitrate (it might or might not answer atomically -> eg queue a snapshot
-  // vote vs a chainlink call) -> atomically should happen during a callback to storeAnswer
-  //
-  // arbitrator can either be a contract which sends dispute resolution to this contract (ie offchain vote later sent)
-  // or a contract which implements IArbitrator
-  // or a contract which resolve as soon as being called (ie chainlink wrapper), in a callback
+  /// @inheritdoc IArbitratorModule
   function startResolution(bytes32 _disputeId) external onlyOracle {
     IOracle.Dispute memory _dispute = ORACLE.getDispute(_disputeId);
 
     address _arbitrator = abi.decode(requestData[_dispute.requestId], (address));
-
-    // Prevent dead-lock if incorrect address
     if (_arbitrator == address(0)) revert ArbitratorModule_InvalidArbitrator();
 
-    // Mark the dispute as Active for the arbitrator
     _disputeData[_disputeId] = 1;
-
-    IArbitrator(_arbitrator).resolve(_disputeId); // Discard the returned value
+    IArbitrator(_arbitrator).resolve(_disputeId);
   }
 
-  // Store the result of an Active dispute and flag it as Resolved
+  /// @inheritdoc IArbitratorModule
   function resolveDispute(bytes32 _disputeId) external onlyOracle {
     IOracle.Dispute memory _dispute = ORACLE.getDispute(_disputeId);
     if (_dispute.status != IOracle.DisputeStatus.Escalated) revert ArbitratorModule_InvalidDisputeId();
@@ -73,11 +58,9 @@ contract ArbitratorModule is Module, IArbitratorModule {
     address _arbitrator = abi.decode(requestData[_dispute.requestId], (address));
     bool _valid = IArbitrator(_arbitrator).getAnswer(_disputeId);
 
-    // Store the answer and the status as resolved
     uint256 _requestDataUpdated = 2 | uint256(_valid ? 1 : 0) << 2;
     _disputeData[_disputeId] = _requestDataUpdated;
 
-    // Call the oracle to update the status
     ORACLE.updateDisputeStatus(_disputeId, _valid ? IOracle.DisputeStatus.Won : IOracle.DisputeStatus.Lost);
   }
 }
--- solidity/interfaces/modules/IArbitratorModule.sol
+++ solidity/interfaces/modules/IArbitratorModule.sol
@@ -3,18 +3,83 @@ pragma solidity ^0.8.19;
 
 import {IResolutionModule} from './IResolutionModule.sol';
 
+/*
+  * @title ArbitratorModule
+  * @notice Module allowing an external arbitrator contract
+  * to resolve a dispute. 
+  */
 interface IArbitratorModule is IResolutionModule {
+  /*///////////////////////////////////////////////////////////////
+                              ERRORS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Thrown when an unauthorized caller calls a function only the arbitrator can call
+   */
   error ArbitratorModule_OnlyArbitrator();
+
+  /**
+   * @notice Thrown when trying to resolve a dispute that is not escalated
+   */
   error ArbitratorModule_InvalidDisputeId();
+
+  /**
+   * @notice Thrown when the arbitrator address is the address zero
+   */
   error ArbitratorModule_InvalidArbitrator();
 
+  /*///////////////////////////////////////////////////////////////
+                              ENUMS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Available status of the arbitration process
+   */
   enum ArbitrationStatus {
-    Unknown,
-    Active,
-    Resolved
+    Unknown, // The arbitration process has not started (default)
+    Active, // The arbitration process is active
+    Resolved // The arbitration process is resolved
   }
 
+  /*///////////////////////////////////////////////////////////////
+                              LOGIC
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Returns the current arbitration status of a dispute
+   * @param _disputeId The ID of the dispute
+   * @return _disputeStatus The `ArbitrationStatus` of the dispute
+   */
   function getStatus(bytes32 _disputeId) external view returns (ArbitrationStatus _disputeStatus);
+
+  /**
+   * @notice Returns the validity of a dispute
+   * @param _disputeId The ID of the dispute
+   * @return _isValid The boolean flag indicating the dispute is valid
+   */
   function isValid(bytes32 _disputeId) external view returns (bool _isValid);
+
+  /**
+   * @notice Starts the arbitration process by calling `resolve` on the
+   * arbitrator and flags the dispute as Active
+   * @dev Only callable by the Oracle
+   * @dev Will revert if the arbitrator address is the address zero
+   * @param _disputeId The ID of the dispute
+   */
+  function startResolution(bytes32 _disputeId) external;
+
+  /**
+   * @notice Resolves the dispute by getting the answer from the arbitrator
+   * and updating the dispute status
+   * @dev Only callable by the Oracle
+   * @param _disputeId The ID of the dispute
+   */
+  function resolveDispute(bytes32 _disputeId) external;
+
+  /**
+   * @notice Returns the decoded data for a request
+   * @param _requestId The ID of the request
+   * @return _arbitrator The address of the arbitrator
+   */
   function decodeRequestData(bytes32 _requestId) external view returns (address _arbitrator);
 }
