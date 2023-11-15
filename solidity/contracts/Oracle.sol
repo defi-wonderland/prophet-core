82db252e7a14b97c0e954f33925311c2a9a80140
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2023-11-15 23:20:13 +0300

    docs: natspec for `_getId`

--- solidity/contracts/Oracle.sol
+++ solidity/contracts/Oracle.sol
@@ -399,15 +399,33 @@ contract Oracle is IOracle {
     emit RequestCreated(_requestId, _request, block.number);
   }
 
-  function _getId(Request calldata _request) internal pure returns (bytes32 _id) {
+  /**
+   * @notice Computes the id a given request
+   *
+   * @param _request The request to compute the id for
+   * @return _id The id the request
+   */
+  function _getId(IOracle.Request calldata _request) internal pure returns (bytes32 _id) {
     _id = keccak256(abi.encode(_request));
   }
 
-  function _getId(Response calldata _response) internal pure returns (bytes32 _id) {
+  /**
+   * @notice Computes the id a given response
+   *
+   * @param _response The response to compute the id for
+   * @return _id The id the response
+   */
+  function _getId(IOracle.Response calldata _response) internal pure returns (bytes32 _id) {
     _id = keccak256(abi.encode(_response));
   }
 
-  function _getId(Dispute calldata _dispute) internal pure returns (bytes32 _id) {
+  /**
+   * @notice Computes the id a given dispute
+   *
+   * @param _dispute The dispute to compute the id for
+   * @return _id The id the dispute
+   */
+  function _getId(IOracle.Dispute calldata _dispute) internal pure returns (bytes32 _id) {
     _id = keccak256(abi.encode(_dispute));
   }
 }
