4aa14c4cc56bc0fe1f685da2ae54cfd4605202fa
Gas <86567384+gas1cent@users.noreply.github.com>
2023-12-04 21:18:21 +0400

    refactor: remove extra dependencies (#13)
    * refactor: remove extra dependencies
    
    * feat: declare forge-std a dev dependency

--- solidity/test/utils/external/IWETH9.sol
+++ solidity/test/utils/external/IWETH9.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.19;
 
-import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {IERC20} from './IERC20.sol';
 
 /// @title Interface for WETH9
 interface IWETH9 is IERC20 {
