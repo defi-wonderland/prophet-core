da407e37b265fe9d6a4175315597aa59e5736cdd
xorsal <zorzal@defi.sucks>
2024-10-13 16:28:05 -0400

    docs: fix IDisputeModule natspec (#50)

--- solidity/interfaces/modules/dispute/IDisputeModule.sol
+++ solidity/interfaces/modules/dispute/IDisputeModule.sol
@@ -11,6 +11,7 @@ interface IDisputeModule is IModule {
 
   /**
    * @notice Emitted when a response is disputed
+   * @param _requestId The id of the request
    * @param _responseId The id of the response disputed
    * @param _disputeId The id of the dispute
    * @param _dispute The dispute that is being created
@@ -32,11 +33,11 @@ interface IDisputeModule is IModule {
   //////////////////////////////////////////////////////////////*/
 
   /**
-   * @notice Called by the oracle when a dispute has been made on a response.
-   * Bonds the tokens of the disputer.
-   * @param _request The id of the response being disputed
-   * @param _response The id of the response being disputed
-   * @param _dispute The id of the response being disputed
+   * @notice Called by the oracle when a dispute has been made on a response
+   * @dev Bonds the tokens of the disputer
+   * @param _request The request
+   * @param _response The disputed response
+   * @param _dispute The dispute
    */
   function disputeResponse(
     IOracle.Request calldata _request,
@@ -47,9 +48,9 @@ interface IDisputeModule is IModule {
   /**
    * @notice Callback executed after a response to a dispute is received by the oracle
    * @param _disputeId The id of the dispute
-   * @param _request The request data
-   * @param _response The disputed response data
-   * @param _dispute The dispute data
+   * @param _request The request
+   * @param _response The disputed response
+   * @param _dispute The dispute
    */
   function onDisputeStatusChange(
     bytes32 _disputeId,
